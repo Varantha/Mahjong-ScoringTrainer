f5ed2643e922b4e7ae0439fffc3db3b251468d5e
duach <ch.robika@gmail.com>
2023-08-03 01:10:46 +0500

    mobile menu overflow issue fixed

--- src/components/OptionsMenu.js
+++ src/components/OptionsMenu.js
@@ -8,67 +8,71 @@ function OptionsMenu(props) {
   const [TooltipOpen, setTooltipOpen] = React.useState(false);
 
   return (
-    <Collapse isOpen={isOpen} className="OptionsMenu">
+
+    <div className="menu-wrap">
+      <Collapse isOpen={isOpen} className="OptionsMenu">
       <h4>Test Options</h4>
-  
+
       <MenuOption
-        options={options}
-        optionId="testHan"
-        optionText="Test Han"
-        changeOptions={props.changeOptions}
-        size="small"
+      options={options}
+      optionId="testHan"
+      optionText="Test Han"
+      changeOptions={props.changeOptions}
+      size="small"
       />
       <MenuOption
-        options={options}
-        optionId="testFu"
-        optionText="Test Fu"
-        changeOptions={props.changeOptions}
-        size="small"
+      options={options}
+      optionId="testFu"
+      optionText="Test Fu"
+      changeOptions={props.changeOptions}
+      size="small"
       />
       <MenuOption
-        options={options}
-        optionId="kiriageMangan"
-        optionText="Kiriage Mangan Mode"
-        changeOptions={props.changeOptions}
-        size="small"
+      options={options}
+      optionId="kiriageMangan"
+      optionText="Kiriage Mangan Mode"
+      changeOptions={props.changeOptions}
+      size="small"
       />
       <br />
       <MenuOption
-        options={options}
-        optionId="testHonba"
-        optionText="Include Honba Sticks"
-        changeOptions={props.changeOptions}
-        size="small"
+      options={options}
+      optionId="testHonba"
+      optionText="Include Honba Sticks"
+      changeOptions={props.changeOptions}
+      size="small"
       />
       <br />
       <div className="link" style={{marginTop:'10px' }}>
-        Log issues on{" "}
-        <a href="https://github.com/Varantha/Mahjong-ScoringTrainer/" style={{ color: "white" }}>GitHub</a>
+      Log issues on{" "}
+      <a href="https://github.com/Varantha/Mahjong-ScoringTrainer/" style={{ color: "white" }}>GitHub</a>
       </div>
       <div style={{ color: "white" , marginTop:'10px' }} >
-        Hand ID: {handName.replace(".json", "")}
+      Hand ID: {handName.replace(".json", "")}
       </div>
       <div className="menuOption large">
-        <button
-          className="checkAnswer"
-          id="applyButton"
-          onClick={(e) => props.changeOptions(e)}
-        >
-          Apply
-        </button>
-        <Tooltip
-          isOpen={TooltipOpen}
-          className="hanTooltip"
-          placement="right"
-          target="applyButton"
-          toggle={() => {
-            setTooltipOpen(!TooltipOpen);
-          }}
-        >
-          Applying these settings will refresh the hand
-        </Tooltip>
+      <button
+      className="checkAnswer"
+      id="applyButton"
+      onClick={(e) => props.changeOptions(e)}
+      >
+      Apply
+      </button>
+      <Tooltip
+      isOpen={TooltipOpen}
+      className="hanTooltip"
+      placement="right"
+      target="applyButton"
+      toggle={() => {
+      setTooltipOpen(!TooltipOpen);
+      }}
+      >
+      Applying these settings will refresh the hand
+      </Tooltip>
       </div>
-    </Collapse>
+      </Collapse>
+    </div>
+   
   );
 }
 
